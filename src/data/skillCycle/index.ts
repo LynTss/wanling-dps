6a9ee14aea98c859b6303ad91d46e0f04e9dcfd5
liyyf <liyyf@tuya.com>
2023-09-18 11:46:54 +0800

    fix: log

--- src/data/skillCycle/index.ts
+++ src/data/skillCycle/index.ts
@@ -1,5 +1,5 @@
 import 九乌, { 循环默认奇穴 as 九乌奇穴 } from './九乌'
-import single_skill_cycle, { 循环默认奇穴 as 常规奇穴 } from './single_skill_cycle'
+// import single_skill_cycle, { 循环默认奇穴 as 常规奇穴 } from './single_skill_cycle'
 
 const All_Cycle_Data = [
   {
@@ -11,14 +11,14 @@ const All_Cycle_Data = [
     type: '九乌',
     qixue: 九乌奇穴,
   },
-  {
-    name: '单技能伤害期望统计-仅参考',
-    title: '单技能伤害期望统计-仅参考',
-    cycle: single_skill_cycle,
-    cycleList: [{ 计算技能数: 1, 循环完整帧数: 1, 循环次数: 1 }],
-    type: '单技能统计',
-    qixue: 常规奇穴,
-  },
+  // {
+  //   name: '单技能伤害期望统计-仅参考',
+  //   title: '单技能伤害期望统计-仅参考',
+  //   cycle: single_skill_cycle,
+  //   cycleList: [{ 计算技能数: 1, 循环完整帧数: 1, 循环次数: 1 }],
+  //   type: '单技能统计',
+  //   qixue: 常规奇穴,
+  // },
 ]
 
 export default All_Cycle_Data
