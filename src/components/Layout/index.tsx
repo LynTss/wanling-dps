3c80715d4f9c1c54cd9d2cd4037c1410480353af
liyyf <liyyf@tuya.com>
2023-10-23 16:31:15 +0800

    fix: 背景图

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 // import ImgBg from '../../assets/main.jpg'
-import ImgBg_1 from '../../assets/bg/1.jpg'
-import ImgBg_2 from '../../assets/bg/2.jpg'
+import ImgBg_1 from '../../assets/bg/1.png'
+import ImgBg_2 from '../../assets/bg/2.png'
+import ImgBg_3 from '../../assets/bg/3.png'
+import ImgBg_4 from '../../assets/bg/4.png'
+import ImgBg_5 from '../../assets/bg/5.png'
+import ImgBg_6 from '../../assets/bg/6.png'
 // import ImgBg_3 from '../../assets/bg/3.jpg'
 import './index.css'
 import { useAppSelector } from '@/hooks'
@@ -15,11 +19,25 @@ const Layout: React.FC<LayoutProps> = (props) => {
   const random = Math.random()
   const closeBackgroundImg = useAppSelector((state) => state?.basic?.closeBackgroundImg)
 
-  const mapKey = random < 0.00001 ? 4 : random < 0.34 ? 1 : random < 0.67 ? 2 : 3
+  const mapKey =
+    random < 1 / 6
+      ? 1
+      : random < 2 / 6
+      ? 2
+      : random < 3 / 6
+      ? 3
+      : random < 4 / 6
+      ? 4
+      : random < 5 / 6
+      ? 5
+      : 6
   const imgMap = {
     1: ImgBg_1,
     2: ImgBg_2,
-    3: ImgBg_1,
+    3: ImgBg_3,
+    4: ImgBg_4,
+    5: ImgBg_5,
+    6: ImgBg_6,
     // 4: ImgBg_18,
   }
 
