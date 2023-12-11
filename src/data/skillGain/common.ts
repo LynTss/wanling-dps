1f5cce6595918d9df0fa1aba8321f6f5b8ae869d
liyyf <liyyf@tuya.com>
2023-12-11 09:47:44 +0800

    fix: 2.0.0

--- src/data/skillGain/common.ts
+++ src/data/skillGain/common.ts
@@ -5,6 +5,18 @@ const 承契基础系数 = 62 / 1024
 
 const commonGainDTO: SkillGainDTO[] = [
   {
+    增益名称: '非侠',
+    增益所在位置: '职业',
+    常驻增益: true,
+    增益集合: [
+      {
+        增益类型: GainTypeEnum.伤害百分比,
+        增益计算类型: GainDpsTypeEnum.D,
+        增益数值: 0.08,
+      },
+    ],
+  },
+  {
     增益名称: '承契_1层',
     增益所在位置: '技能',
     增益启用开关: true,
