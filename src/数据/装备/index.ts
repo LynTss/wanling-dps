0a4f1a0091be73d08890d4a1fbbe8455ef9e0dfe
liyyf <liyyf@tuya.com>
2024-04-22 10:30:32 +0800

    feat: :sparkles: 新增新赛季装备库

--- src/数据/装备/index.ts
+++ src/数据/装备/index.ts
@@ -13,18 +13,17 @@ import ZHUANGBEI_JINSHENWUQI from './jinshenwuqi'
 // TODO 龙门武器id
 export const 龙门_武器: EquipmentIds = [36911]
 // 特效武器
-export const 特效_武器_普通: EquipmentIds = [36660, 36657]
-export const 特效_武器_英雄: EquipmentIds = [35781]
+export const 特效_武器_普通: EquipmentIds = [35781]
+export const 特效_武器_英雄: EquipmentIds = [37252]
 // 特效腰坠
-export const 特效_腰椎_普通: EquipmentIds = [37720]
-export const 特效_腰椎_英雄: EquipmentIds = [38791]
+export const 特效_腰椎_普通: EquipmentIds = [38791]
+export const 特效_腰椎_英雄: EquipmentIds = [39855]
 // 切糕套装
-export const 切糕套装_普通: EquipmentIds = [94464, 94488, 94482, 94476, 94470, 94494]
-
-export const 切糕套装_英雄: EquipmentIds = [96367, 96391, 96385, 96379, 96373, 96397]
+export const 切糕套装_普通: EquipmentIds = [96367, 96391, 96385, 96379, 96373, 96397]
+export const 切糕套装_英雄: EquipmentIds = [98481, 98505, 98499, 98493, 98487, 98511]
 // 大CW特效Ids
 export const 大橙武武器: EquipmentIds = [
-  33133, 34369, 34399, 34429, 35593, 35623, 36796, 36797, 36804,
+  33133, 34369, 34399, 34429, 35593, 35623, 36796, 36797, 38046, 38077,
 ]
 // export const 大CW特效Ids: EquipmentIds = []
 export const 小橙武武器: EquipmentIds = [36807, 37073, 37104]
@@ -35,7 +34,7 @@ export const 冬至套装: EquipmentIds = [36859, 39791, 98157, 98127]
 // 2件套会心 4件套 饮羽10%
 export const 套装_两件套双会: EquipmentIds = [96135, 96225, 96195, 96165, 96255]
 // 2件套 饮羽10% 4件套会心
-export const 套装_两件套技能: EquipmentIds = [97846, 97849, 97848, 97847, 97850]
+export const 套装_两件套技能: EquipmentIds = [98249, 98339, 98309, 98279, 98369]
 
 const ZUANGBEI_DATA = {
   帽子: ZHUANGBEI_MAOZI,
